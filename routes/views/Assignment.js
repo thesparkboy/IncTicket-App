a2dd54124668c8e30af99b42898599efa3e6f8a1
Aakash Madan <madan.aakash2@gmail.com>
2017-06-03 15:26:14 +0530

    Delete Assignment.js

--- routes/views/Assignment.js
+++
@@ -1,8 +0,0 @@
-var keystone = require('keystone');
-
-exports = module.exports = function (req, res) {
-
-	var view = new keystone.View(req, res);
-	// Render the view
-	view.render('Assignment');
-};
